14686b01ca383f85e0e504594811a9da5c36575e
vanyavorobiov <s19b1_vorobiov@179.ru>
2024-10-30 02:55:47 +0300

    changed known ids to password cheking

--- bot.py
+++ bot.py
@@ -3,66 +3,56 @@ from telegram import Update, KeyboardButton, ReplyKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 from minecraft import MinecraftServer
 from secret_token import TOKEN
-from config import TIME_LOGIN_AVAILABILITY, ALLOWED_USER_IDS
+from secret_password import PASSWORD
+from config import TIME_LOGIN_AVAILABILITY
 
-
-# Dictionary for storing registered users' nicknames
-user_nicks = {}
 
 minecraft_server = MinecraftServer()
 
+id_to_nickname = {}
 
 # Command /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    try:
-        user_id = update.effective_chat.username
-    except:
-        user_id = update.effective_chat.id
+    await update.message.reply_text("Enter the password to access the registration:")
+    return "WAITING_FOR_PASSWORD"
 
-    if user_id in ALLOWED_USER_IDS:
+
+# Handling password input
+async def check_password(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    entered_password = update.message.text
+    if entered_password == PASSWORD:
         button = KeyboardButton("Register")
         reply_markup = ReplyKeyboardMarkup([[button]], resize_keyboard=True)
-        await update.message.reply_text("Choose a button:", reply_markup=reply_markup)
+        await update.message.reply_text("Password correct. Choose a button:", reply_markup=reply_markup)
     else:
-        await update.message.reply_text("To use the bot, send your username to @v0r0bi0v. " + 
-                                        "If your username is hidden, send your ID, which you can find in @userinfobot.")
+        await update.message.reply_text("Incorrect password. Please try again by sending /start.")
+
 
 # Handling button press for Register
 async def register(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("Enter your nickname:")
     return "WAITING_FOR_NICK"
 
+
 # Handling the entered nickname
 async def handle_nick(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    try:
-        user_id = update.effective_chat.username
-    except:
-        user_id = update.effective_chat.id
-    nick = update.message.text
-    user_nicks[user_id] = nick
-
+    id_to_nickname[update.effective_chat.id] = update.message.text
     button = KeyboardButton("Login")
     reply_markup = ReplyKeyboardMarkup([[button]], resize_keyboard=True)
     await update.message.reply_text("Your nickname has been saved. Press the button to log in:", reply_markup=reply_markup)
 
+
 # Handling button press for Login
 async def login(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    try:
-        user_id = update.effective_chat.username
-    except:
-        user_id = update.effective_chat.id
+    await update.message.reply_text("You have two minutes to log in to the server.")
+    minecraft_server.add_to_whitelist(id_to_nickname[update.effective_chat.id])
+    # Wait for 2 minutes without blocking
+    await asyncio.sleep(TIME_LOGIN_AVAILABILITY)
+    # After 2 minutes, show the Login button again
+    button = KeyboardButton("Login")
+    reply_markup = ReplyKeyboardMarkup([[button]], resize_keyboard=True)
+    await update.message.reply_text("Time is up. Press the button to log in again:", reply_markup=reply_markup)
 
-    if user_id in user_nicks:
-        await update.message.reply_text("You have two minutes to log in to the server.")
-        minecraft_server.add_to_whitelist(user_nicks[user_id])
-        # Wait for 2 minutes without blocking
-        await asyncio.sleep(TIME_LOGIN_AVAILABILITY)
-        # After 2 minutes, show the Login button again
-        button = KeyboardButton("Login")
-        reply_markup = ReplyKeyboardMarkup([[button]], resize_keyboard=True)
-        await update.message.reply_text("Time is up. Press the button to log in again:", reply_markup=reply_markup)
-    else:
-        await update.message.reply_text("Please register first!")
 
 async def main() -> None:
     # Create the bot
@@ -70,6 +60,7 @@ async def main() -> None:
 
     # Commands
     app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & filters.Regex(PASSWORD), check_password))
     app.add_handler(MessageHandler(filters.Regex('^Register$'), register))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_nick))
     app.add_handler(MessageHandler(filters.Regex('^Login$'), login))
@@ -78,10 +69,4 @@ async def main() -> None:
     await app.run_polling()
 
 if __name__ == '__main__':
-    import asyncio
-
-    try:
-        asyncio.run(main())
-    except RuntimeError:  # If there's an existing loop running
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(main())
+    asyncio.run(main())
